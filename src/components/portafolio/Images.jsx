c6190ecbd74c3057f6fdbd256ea1442d43269336
D3R5 <diegoesteban.ders@gmail.com>
2023-11-03 17:31:58 -0300

    change -portfolio -2 new components

---
+++ src/components/portafolio/Images.jsx
@@ -0,0 +1,42 @@
+// import IMG1 from '../../assets/portfolio1.jpg';
+// import IMG2 from '../../assets/portfolio2.jpg';
+// import IMG3 from '../../assets/portfolio3.jpg';
+// import IMG4 from '../../assets/portfolio4.jpg';
+// import IMG5 from '../../assets/portfolio5.png';
+// import IMG6 from '../../assets/portfolio6.jpg';
+import IMG7 from "../../assets/emmart.png";
+import IMG8 from "../../assets/tukimarket.png";
+import IMG9 from "../../assets/apimovie.png";
+import IMG10 from "../../assets/qrgenerator.png";
+import IMG11 from "../../assets/calculator.png";
+import IMG12 from "../../assets/webresponsive.png";
+import IMG14 from "../../assets/id.png";
+import IMG15 from "../../assets/port.png";
+import IMG16 from "../../assets/instagram.png";
+import IMG17 from "../../assets/monogram.png";
+import IMG18 from "../../assets/minecraftClon.png";
+import IMG19 from "../../assets/calculadoraIntellij.png";
+import IMG20 from "../../assets/listadoPersonasIntellij.png";
+import IMG21 from "../../assets/ventaPcIntellij.png";
+import IMG22 from "../../assets/catalogoPeliculasIntellij.png";
+
+const images = {
+    img7: IMG7,
+    img8: IMG8,
+    img9: IMG9,
+    img10: IMG10,
+    img11: IMG11,
+    img12: IMG12,
+    img14: IMG14,
+    img14: IMG14,
+    img15: IMG15,
+    img16: IMG16,
+    img17: IMG17,
+    img18: IMG18,
+    img19: IMG19,
+    img20: IMG20,
+    img21: IMG21,
+    img22: IMG22,
+}
+
+export default images;
--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -1,39 +1,19 @@
 import React from "react";
 import "./portafolio.css";
-// import IMG1 from '../../assets/portfolio1.jpg';
-// import IMG2 from '../../assets/portfolio2.jpg';
-// import IMG3 from '../../assets/portfolio3.jpg';
-// import IMG4 from '../../assets/portfolio4.jpg';
-// import IMG5 from '../../assets/portfolio5.png';
-// import IMG6 from '../../assets/portfolio6.jpg';
-import IMG7 from "../../assets/emmart.png";
-import IMG8 from "../../assets/tukimarket.png";
-import IMG9 from "../../assets/apimovie.png";
-import IMG10 from "../../assets/qrgenerator.png";
-import IMG11 from "../../assets/calculator.png";
-import IMG12 from "../../assets/webresponsive.png";
-import IMG14 from "../../assets/id.png";
-import IMG15 from "../../assets/port.png";
-import IMG16 from "../../assets/instagram.png";
-import IMG17 from "../../assets/monogram.png";
-import IMG18 from "../../assets/minecraftClon.png";
-import IMG19 from "../../assets/calculadoraIntellij.png";
-import IMG20 from "../../assets/listadoPersonasIntellij.png";
-import IMG21 from "../../assets/ventaPcIntellij.png";
-import IMG22 from "../../assets/catalogoPeliculasIntellij.png";
+import images from "./Images";
 
 const Portafolio = () => {
   const data = [
     {
       id: 1,
-      image: IMG7,
+      image: images.img7,
       title: "Simple Ecommerce",
       github: "https://github.com/D3R5/emmart",
       demo: "https://emmart.vercel.app",
     },
     {
       id: 2,
-      image: IMG8,
+      image: images.img8,
       title: "Marketplace",
       github1: "https://github.com/D3R5/Frontend-TukiMarket",
       github2: "https://github.com/D3R5/Backend-TukiMarket",
@@ -41,82 +21,82 @@ const Portafolio = () => {
     },
     {
       id: 3,
-      image: IMG15,
+      image: images.img9,
       title: "My Portfolio - React",
       github: "https://github.com/D3R5/My-portfolio",
       demo: "https://my-portfolio-orpin-kappa.vercel.app",
     },
     {
       id: 4,
-      image: IMG16,
+      image: images.img10,
       title: "Instagram Clone - React/Redux/Firebase",
       github: "https://github.com/D3R5/instagram-clone-react",
       demo: "https://instagram-clone-ebon-gamma.vercel.app",
     },
     {
       id: 5,
-      image: IMG17,
+      image: images.img11,
       title: "Monogram Console, first page (Clon)",
       github: "https://github.com/D3R5/monogramConsoleClon",
       demo: "https://monogram-console-clon.vercel.app",
     },
     {
       id: 6,
-      image: IMG14,
+      image: images.img12,
       title: "Api Rest - NodeJs, Express, Typescript",
       github: "https://github.com/D3R5/Api-rest",
     },
     {
       id: 7,
-      image: IMG9,
+      image: images.img14,
       title: "Movie-Api",
       github: "https://github.com/D3R5/Movie-Api",
     },
     {
       id: 8,
-      image: IMG10,
+      image: images.img15,
       title: "QR-Generator",
       github: "https://github.com/D3R5/Qr-Generator",
     },
     {
       id: 9,
-      image: IMG11,
+      image: images.img16,
       title: "CalculatorReactJS",
       github: "https://github.com/D3R5/CalculatorReactJs",
     },
     {
       id: 10,
-      image: IMG12,
+      image: images.img17,
       title: "Simple Web Responsive",
       github: "https://github.com/D3R5/WebResponsive",
     },
     {
       id: 11,
-      image: IMG18,
+      image: images.img18,
       title: "Minecraft Clon React/ThreeJs",
       github: "https://github.com/D3R5/minecraftClonJs",
     },
     {
       id: 12,
-      image: IMG19,
+      image: images.img19,
       title: "Calculator Intellij Java",
       github: "https://github.com/D3R5/Intellij_Calculator",
     },
     {
       id: 13,
-      image: IMG20,
+      image: images.img20,
       title: "People List Intellij Java",
       github: "https://github.com/D3R5/Intellij_PeopleList",
     },
     {
       id: 14,
-      image: IMG21,
+      image: images.img21,
       title: "Computer Sale Intellij Java",
       github: "https://github.com/D3R5/Intellij_ComputersSale",
     },
     {
       id: 15,
-      image: IMG22,
+      image: images.img22,
       title: "Movies Intellij Java",
       github: "https://github.com/D3R5/Intellij_Movies",
     },
