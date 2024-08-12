91dbde43b70903484795fed42a0f389b24df80d0
D3R5 <diegoesteban.ders@gmail.com>
2024-08-12 13:03:44 -0400

    change header add en es - add cv en es

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import CV from "../../assets/Diego_Rivera_Sprohnle_CV_english.pdf";
+import CV_en from "../../assets/Diego_Rivera_Sprohnle_CV_english.pdf";
+import CV_es from "../../assets/Diego_Rivera_Sprohnle_CV.pdf";
+import { useTranslation } from "react-i18next";
 
 const CTA = () => {
+  const { t, i18n } = useTranslation();
+
+  // Determina el CV a descargar según el idioma actual
+  const cvFile = i18n.language === 'en' ? CV_en : CV_es;
+
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download CV
+      <a href={cvFile} download className="btn">
+        {t("download_cv")}
       </a>
       <a href="#contact" className="btn btn-primary">
-        Let's Talk
+        {t("lets_talk")}
       </a>
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,24 @@ import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/perfil.jpg";
 import HeaderSocials from "./HeaderSocials";
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
     <header>
       <div className="container header__container">
-        <h5>console.log('Hello World!!🌎🖖'); //I'm:</h5>
+        <h5>{t("console_log")}</h5>
         <h1>Diego Rivera Spröhnle</h1>
-        <h5 className="text light">Fullstack Developer</h5>
+        <h5 className="text light">{t("title_fullstack")}</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
           <img src={ME} alt="" />
         </div>
         <a href="#contact" className="scroll__down">
-          Scroll Down
+          {t("scroll_down")}
         </a>
       </div>
     </header>
