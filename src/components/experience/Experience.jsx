4857fa3e76d5f69c025c8420999fb7d141b46987
D3R5 <diegoesteban.ders@gmail.com>
2024-08-12 12:32:31 -0400

    change experience add en es

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./experience.css";
 import { BsPatchCheckFill } from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 const Experience = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="experience">
-      <h5>What Skills I Have</h5>
-      <h2>My Experience</h2>
+      <h5>{t("skills")}</h5>
+      <h2>{t("my_experience")}</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
-          <h3>Frontend Development</h3>
+          <h3>{t("frontend_development")}</h3>
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
@@ -71,7 +74,7 @@ const Experience = () => {
           </div>
         </div>
         <div className="experience__backend">
-          <h3>Backend Development</h3>
+          <h3>{t("backend_development")}</h3>
           <div className="experience__content">
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
