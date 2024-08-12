408c55c6967d1fb3bcce989bc744d38879f8a862
D3R5 <diegoesteban.ders@gmail.com>
2024-08-12 13:19:32 -0400

    change services add en es

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,118 +1,121 @@
 import React from "react";
 import "./services.css";
 import { BiCheck } from "react-icons/bi";
+import { useTranslation } from "react-i18next";
 
 const Services = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="services" className="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>{t("services_subtitle")}</h5>
+      <h2>{t("services_title")}</h2>
 
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>{t("services_uiux_title")}</h3>
           </div>
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>User research and analysis</p>
+              <p>{t("services_uiux_item1")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>User interface (UI) design</p>
+              <p>{t("services_uiux_item2")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>User experience (UX) design</p>
+              <p>{t("services_uiux_item3")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Prototyping and Wireframing</p>
+              <p>{t("services_uiux_item4")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Usability testing and user feedback</p>
+              <p>{t("services_uiux_item5")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Responsive and adaptive design</p>
+              <p>{t("services_uiux_item6")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Integration and collaboration with development teams</p>
+              <p>{t("services_uiux_item7")}</p>
             </li>
           </ul>
         </article>
 
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>{t("services_webdev_title")}</h3>
           </div>
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Custom Website Development.</p>
+              <p>{t("services_webdev_item1")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Web application development.</p>
+              <p>{t("services_webdev_item2")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Responsive website development.</p>
+              <p>{t("services_webdev_item3")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>System integration and API.</p>
+              <p>{t("services_webdev_item4")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>E-commerce development.</p>
+              <p>{t("services_webdev_item5")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Performance and speed optimization.</p>
+              <p>{t("services_webdev_item6")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Maintenance and technical support.</p>
+              <p>{t("services_webdev_item7")}</p>
             </li>
           </ul>
         </article>
 
         <article className="service">
           <div className="service__head">
-            <h3>Content Creation</h3>
+            <h3>{t("services_content_title")}</h3>
           </div>
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Writing articles and blogs.</p>
+              <p>{t("services_content_item1")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Design of infographics and visual material.</p>
+              <p>{t("services_content_item2")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Content writing for websites and landing pages.</p>
+              <p>{t("services_content_item3")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Audio recording and editing.</p>
+              <p>{t("services_content_item4")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Interpretation and live performances.</p>
+              <p>{t("services_content_item5")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Collaborations and study sessions.</p>
+              <p>{t("services_content_item6")}</p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Consulting and musical advice.</p>
+              <p>{t("services_content_item7")}</p>
             </li>
           </ul>
         </article>
